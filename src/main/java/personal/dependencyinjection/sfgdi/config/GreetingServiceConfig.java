baef021a9b27e5c65f65188a2c26d54579bd26d5
ArnoldasG <grevasarnoldas@gmail.com>
2021-04-30 18:50:35 +0300

    Removed @Service anr @Primary annotations from i18 classes, and declared as Beans as Java Configuration.

--- src/main/java/personal/dependencyinjection/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/personal/dependencyinjection/sfgdi/config/GreetingServiceConfig.java
@@ -2,13 +2,31 @@ package personal.dependencyinjection.sfgdi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import personal.dependencyinjection.sfgdi.services.ConstructorGreetingService;
-import personal.dependencyinjection.sfgdi.services.PropertyInjectedGreetingService;
-import personal.dependencyinjection.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import personal.dependencyinjection.sfgdi.services.*;
 
 @Configuration // This tells to component scan that there is a configuration class and this is going
 // to define a different bean
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishService i18nSpanishService(){
+        return new I18nSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishService i18nService() {
+        return new I18nEnglishService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
--- src/main/java/personal/dependencyinjection/sfgdi/services/I18nEnglishService.java
+++ src/main/java/personal/dependencyinjection/sfgdi/services/I18nEnglishService.java
@@ -3,9 +3,8 @@ package personal.dependencyinjection.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
-@Service("i18nService")
-
+//@Profile("EN")
+//@Service("i18nService")
 public class I18nEnglishService implements GreetingService {
 
     @Override
